664d1a0e37f8c311505ba09dff9b578d2504c797
ImogenConnorHelleur <imooconnor@outlook.com>
2017-09-08 13:01:05 +0100

    Added lines to load data files.

--- R/load_data.R
+++ R/load_data.R
@@ -1,5 +1,6 @@
 
 # Either return the argument x or raise it to the power 4
+
 my_function <- function(x, ident = FALSE) {
   
   if(ident){ 
@@ -9,5 +10,9 @@ my_function <- function(x, ident = FALSE) {
   }
   
   y
-  
 }
+
+# Loads data files
+
+  load("data/admission_data_clean.rda")
+  load("data/emergency_adms.rda")
