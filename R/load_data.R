8f6b61d72a928ea65cb5f2d3f555b6b81eaafdf6
ImogenConnorHelleur <imooconnor@outlook.com>
2017-09-12 13:48:34 +0100

    Added function to load libraries.

--- R/load_data.R
+++ R/load_data.R
@@ -1,21 +1,16 @@
 
-# Either return the argument x or raise it to the power 4
-
-my_function <- function(x, ident = FALSE) {
-  
-  if(ident){ 
-    y <- x
-  } else { 
-    y <- x^4
-  }
-  
-  y
-}
-
 # Loads data files into Global Environment
 # Note: this will overwrite objects with the same names
 # in the Global Environment.
 load_data <- function() {
   load("data/admission_data_clean.rda", .GlobalEnv)
   load("data/emergency_adms.rda", .GlobalEnv)
+}
+
+
+# loads libraries
+load_libraries <- function(){
+  library(lubridate)
+  library(ggplot2)
+  library(testthat)
 }
