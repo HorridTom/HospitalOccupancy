ed8c1ec4f7ca2e13aabc1ed0c820e6c30e27f79a
ImogenConnorHelleur <imooconnor@outlook.com>
2017-09-13 14:57:01 +0100

    Adapted test for hospital_occupancy in line with changed arguments in function. Added test for ward_occupancy.

--- tests/test_occupancy.R
+++ tests/test_occupancy.R
@@ -1,24 +1,50 @@
-#test for occupancy function 
+#test for hospital occupancy function 
 
-test_that("Test Occupancy", {
+test_that("Test hospital_occupancy", {
    
    #set up a much smaller data frame with known occupancies at given test_times
    test_pseudoID <- 1:5
    test_CSPAdmissionTime <- c("2011-01-01 00:00:00", "2011-01-02 00:00:00", "2011-01-01 00:00:00", "2011-01-05 00:00:00", "2011-01-05 00:00:00")
    test_CSPDischargeTime <- c("2011-01-03 12:00:00", "2011-01-03 23:59:59 ", "2011-01-02 12:00:00", "2011-01-05 12:00:00", "2011-05-05 23:59:59")
-   test_Stay <- interval(test_CSPAdmissionTime, test_CSPDischargeTime)
    test_df <- data.frame(test_pseudoID, test_CSPAdmissionTime, test_CSPDischargeTime, test_Stay)
    test_times <- c("2011-01-01 15:00:00:00", "2011-01-02 10:00:00", "2011-01-03 15:00:00", "2011-01-04 15:00:00", "2011-01-05 10:00:00")
    
-   #run occupancy
-   test_occupancy1 <- as.numeric(sapply(test_times, hospital_occupancy, df = test_df, df_col = "test_Stay"))
+   #run hospital occupancy
+   test_occupancy1 <- as.numeric(sapply(test_times, hospital_occupancy, df = test_df, time_in_col = "test_CSPAdmissionTime", time_out_col = "test_CSPDischargeTime"))
    
    #correct occupancy
    correct_occupancy1 <- c(2, 3, 1, 0, 2)
    
    #check results
    expect_equal(test_occupancy1, correct_occupancy1)
+   
   })
  
  
- 
+
+
+#test for ward occupancy function
+
+test_that("Test ward_occupancy", {
+  
+  #set up a much smaller data frame with known occupancies at given test_times
+  test_pseudoID <- 1:5
+  test_EpisodeStartDate <- c("2011-01-01 00:00:00", "2011-01-02 00:00:00", "2011-01-01 00:00:00", "2011-01-05 00:00:00", "2011-01-05 00:00:00")
+  test_EpisodeEndDate <- c("2011-01-03 12:00:00", "2011-01-03 23:59:59 ", "2011-01-02 12:00:00", "2011-01-05 12:00:00", "2011-05-05 23:59:59")
+  test_ward <- c("BLUE_ward", "ORANGE_ward", "ORANGE_ward", "BLUE_ward", "BLUE_ward")
+  test_df <- data.frame(test_pseudoID, test_EpisodeStartDate, test_EpisodeEndDate, test_ward)
+  test_times <- c("2011-01-01 15:00:00:00", "2011-01-02 10:00:00", "2011-01-03 15:00:00", "2011-01-04 15:00:00", "2011-01-05 10:00:00")
+  
+  #run ward occupancy
+  test_occupancy_orange <- as.numeric(sapply(test_times, ward_occupancy, ward = "ORANGE_ward", df = test_df, time_in_col = "test_EpisodeStartDate", time_out_col = "test_EpisodeEndDate", ward_col = "test_ward"))
+  test_occupancy_blue <- as.numeric(sapply(test_times, ward_occupancy, ward = "BLUE_ward", df = test_df, time_in_col = "test_EpisodeStartDate", time_out_col = "test_EpisodeEndDate", ward_col = "test_ward"))
+  
+  #correct occupancy
+  correct_occupancy_orange <- c(1, 2, 1, 0, 0)
+  correct_occupancy_blue <- c(1, 1, 0, 0, 2)
+  
+  #check results
+  expect_equal(test_occupancy_orange, correct_occupancy_orange)
+  expect_equal(test_occupancy_blue, correct_occupancy_blue)
+  
+})
